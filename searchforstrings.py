a9c20ea5f54bc22b3c75decf6e4849473bcca191
jonathan1440 <jonathan@ziesmer.us>
2017-09-22 20:32:23 -0400

    searchforstrings.py now writes files
    It should now write the results for each book into its own file at a specified location. Still need to test it...

--- searchforstrings.py
+++ searchforstrings.py
@@ -7,41 +7,40 @@ print("This script will take a folder of .txt files and search them for strings 
 print('')
 print("This script will store the results for each book in an individual file.")
 print("What path should the file be created under?")
-print("**Note: if the path includes the folder this script is under, you can start at that level. Otherwise you need to start at the top.")
+print("**Note: if the path includes the folder this script is under, you can start at that folder's level. Otherwise you need to start at the top.")
 resultspath = str(input(": "))
-
+ 
 #file path for dir with the files with book text to search through
 print("")
-bookspath = str(input("Folder path: "))
+bookspath = str(input("Path to folder with text of books to search: "))
 
 #file path for file with the strings to find
 print("")
-sourcepath = str(input("Source .txt path: "))
+sourcepath = str(input("Path to file with strings to search for: "))
 
 #start timer
 starttime = time.gmtime()
 
-#array for storing search results
-results = []
-
 #get strings to find in books
 f = open(sourcepath, 'r')
-#f.readlines() returns an array of all the lines in the text file f
+#f.readlines() returns a list of all the lines in the text file f
 stringstofind = f.readlines()
+#always .close() a file after you are done using it
 f.close()
 
 #get books
 booktextfiles = []
-#os.walk returns an array something along the lines of [dir path, names of dirs in dir, names of files in dir]
+#os.walk returns a list something along the lines of [dir path, names of dirs in dir, names of files in dir]
 for (dirpath,dirnames,filenames) in os.walk(bookspath):
-    #add the filenames to datafiles
+    #add the filenames of the files with the book texts to datafiles
     booktextfiles.extend(filenames)
+#----<----
     
 #This function exists solely so I could use recursion :)
-#Also to return multiple indices if possible, instead of the one returned by .find(
+#Also to return multiple indices if possible, instead of just one returned by .find()
 def findString (strtosearch,strtofind):
     #store indices of occurances
-    num = []
+    indices = []
 
     def fs (strtosearch,strtofind,startIndex):
         #look for first occurance, store index as ind
@@ -49,20 +48,24 @@ def findString (strtosearch,strtofind):
 
         #if occurance occured,
         if ind != -1:
-            #store index outside
-            num.append(ind)
-            #find next index with adjusted start index
+            #store index outside function
+            indices.append(ind)
+            #find next index adjusted to start after the index of the previous occurance
             fs(strtosearch,strtofind,ind+1)
+        #----<----
+    #----<----
 
-    #find all occurances
+    #implement fs() to find all occurances
     fs(strtosearch,strtofind,0)
 
     #return indices of occurances    
-    return num
+    return indices
+#----<----
     
-#for storing performance data
+#for storing performance data, will be a dict
 perdata = []
     
+#counter
 booknum = 0
 #for each book...
 while booknum < len(booktextfiles):
@@ -74,43 +77,66 @@ while booknum < len(booktextfiles):
     booklines = f.readlines()
     f.close()
     
-    #create/open a file for storing results from this book
+    #create or open a file for storing results from this book
     results = open(resultspath+"\\"+booktextfiles[booknum]+"results.txt","w")
     
+    #counter
     linenum = 0
-    #for each line in book...
+    #for each line in book text file...
     while linenum < len(booklines):
-        #create element to temporarily store book's element for this line's data
+        #create list to temporarily store book's element for this line's data
         result = []
         
-        #for each string to find...
+        #for each string to search for...
         for string in stringstofind:
-            #find string i in string of line linenum
+            #find string "string" in string of line "linenum"
             locations = findString(booklines[linenum],string)
 
-            #if any occurances were actually found...
+            #if any occurances were found...
             if len(locations) > 0:
                 #add them to the results list
                 result.append([string[0:len(string)-1],locations])
+            #----<----
                 
             #to help avoid crashing? IDK if this actually helps or not
             time.sleep(0.01)
+        #----<----
             
-        #if any results, add them to the file
+        #if any results, add them to the results file
         if len(result) > 0:
-            results.write(linenum)
+            results.write(str(linenum))
+            
+            #for each string to search for...
             for i in range(len(result)):
-                for j in range(len(result[i])):
-                    
+                #create empty variable to store everythign I want on one line in the results file
+                #unfortunately the .write() function only takes strings, so...
+                writeline = ""
+                #add the particular string to line
+                writeline.join(result[i][0])
+                #using str.join() is more efficient than += when compiling
+                writeline.join(": ")
+                
+                #for each index in the line where the string occured...
+                for j in range(len(result[i][1])):
+                    #add the index to the line
+                    writeline.join(result[i][1][j])
+                    writeline.join(", ")
+                #----<----
+                
+                #write the line to the results file
+                results.write(writeline)
+            #----<----
+        #----<----
         
         #next line in book
-        linenum = linenum + 1        
+        linenum += 1 
+    #----<----
     
     #close results file
     results.close()
     
     #next book
-    booknum = booknum +1
+    booknum += 1
     
     #end book timer
     perdata.append([])
@@ -124,5 +150,8 @@ while booknum < len(booktextfiles):
     results.write("")
     for name,data in perdata.items():
         results.write(name+" : "+str(data))
+    #----<----
+#----<----
 
-print(results)
+
+
